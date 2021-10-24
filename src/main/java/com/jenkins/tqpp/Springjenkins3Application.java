84cb2651387e97b9ded0394387b80f013830eef9
deepa <tqdeeepa@gmail.com>
2021-10-24 05:45:05 +0530

    first Commit

---
+++ src/main/java/com/jenkins/tqpp/Springjenkins3Application.java
@@ -0,0 +1,13 @@
+package com.jenkins.tqpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springjenkins3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springjenkins3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkins/tqpp/Springjenkins3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.tqpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springjenkins3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
